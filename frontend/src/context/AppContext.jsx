7552068454dd9796017a8a0d06a44a1df4af5fb3
RohitProcoderr <ruderohit833@gmail.com>
2026-03-03 20:11:46 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
-axios.defaults.baseURL = "https://luxurycarrentalsite-backend.onrender.com"c;
+axios.defaults.baseURL = "https://luxurycarrentalsite-backend.onrender.com";
 
 export const AppContext = createContext();
 
